8b762feed31643291beafb0d9d805245ed402ed5
Celine <ly.celine@hotmail.fr>
2022-11-06 11:13:43 +0100

    3rd day part 1 and 2

--- aoc_2016/src/bin/main_2016_03.rs
+++ aoc_2016/src/bin/main_2016_03.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -7,4 +8,58 @@ fn main() {
 
     file.read_to_string(&mut data)
         .expect("Error while reading file");
+
+    // first part
+    let all_triangles: Vec<[u16; 3]> = data
+        .lines()
+        .map(|line| {
+            let re = Regex::new(r"[ ]*([0-9]*)[ ]*([0-9]*)[ ]*([0-9]*)").unwrap();
+            let caps = re.captures(line).unwrap();
+
+            [
+                caps.get(1).unwrap().as_str().parse::<u16>().unwrap(),
+                caps.get(2).unwrap().as_str().parse::<u16>().unwrap(),
+                caps.get(3).unwrap().as_str().parse::<u16>().unwrap(),
+            ]
+        })
+        .collect();
+
+    let real_triangles = all_triangles
+        .iter()
+        .filter(|&triangle| {
+            let mut sorted_triangle = triangle.clone();
+            sorted_triangle.sort();
+            sorted_triangle[0] + sorted_triangle[1] > sorted_triangle[2]
+        })
+        .count();
+
+    println!("{:?} of the listed triangles are possible", real_triangles);
+
+    // second part
+    let mut all_v_triangles: Vec<[u16; 3]> = Vec::new();
+    (0..=all_triangles.len() - 3).for_each(|row| {
+        if row % 3 == 0 || row == 0 {
+            (0..3).for_each(|column| {
+                all_v_triangles.push([
+                    all_triangles[row][column],
+                    all_triangles[row + 1][column],
+                    all_triangles[row + 2][column],
+                ]);
+            });
+        }
+    });
+
+    let real_v_triangles = all_v_triangles
+        .iter()
+        .filter(|&triangle| {
+            let mut sorted_triangle = triangle.clone();
+            sorted_triangle.sort();
+            sorted_triangle[0] + sorted_triangle[1] > sorted_triangle[2]
+        })
+        .count();
+
+    println!(
+        "reading by columns, {:?} of the listed triangles are possible",
+        real_v_triangles
+    );
 }
