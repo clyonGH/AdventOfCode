a2b0bb1d6fc0d72671c08ecb3e88df59821456ae
Celine <ly.celine@hotmail.fr>
2022-12-04 12:37:06 +0100

    4th day part 1 and 2

---
+++ aoc_2022/src/bin/main_2022_04.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let mut file = File::open("inputs/input_2022_04.txt").expect("Error: File not found");
+    let mut data: String = String::new();
+
+    file.read_to_string(&mut data)
+        .expect("Error while reading file");
+
+    let split_pairs: Vec<(u16, u16, u16, u16)> = data
+        .lines()
+        .map(|line| {
+            let split_pair: Vec<&str> = line.split(',').collect();
+            let split_e1: Vec<&str> = split_pair[0].split('-').collect();
+            let split_e2: Vec<&str> = split_pair[1].split('-').collect();
+
+            (
+                split_e1[0].parse::<u16>().unwrap(),
+                split_e1[1].parse::<u16>().unwrap(),
+                split_e2[0].parse::<u16>().unwrap(),
+                split_e2[1].parse::<u16>().unwrap(),
+            )
+        })
+        .collect();
+
+    // first part
+    let nb_pairs: Vec<&(u16, u16, u16, u16)> = split_pairs
+        .iter()
+        .filter(|(e1min, e1max, e2min, e2max)| {
+            (e1min <= e2min && e1max >= e2max) || (e2min <= e1min && e2max >= e1max)
+        })
+        .collect();
+
+    println!(
+        "{:?} assignment pairs have one range fully containing the other",
+        nb_pairs.iter().count()
+    );
+
+    // second part
+    let overlapping_pairs: Vec<&(u16, u16, u16, u16)> = split_pairs
+        .iter()
+        .filter(|(e1min, e1max, e2min, e2max)| {
+            (e1max >= e2min && e1min <= e2max) || (e2max >= e1min && e2min <= e1max)
+        })
+        .collect();
+
+    println!(
+        "{:?} assignment pairs have ranges that overlap",
+        overlapping_pairs.iter().count()
+    );
+}
